744601ac957f1fbb1d43db1314932fc627f2ccfc
Corgiboy <dmitryholmes@yandex.ru>
2017-04-20 22:08:12 +0300

    Начало работы над nbk.

---
+++ da/CP/main.cpp
@@ -0,0 +1,13 @@
+#include "classes.hpp"
+#include "training.hpp"
+#include "classifier.hpp"
+
+int main(int argc, char const *argv[]) {
+
+    size_t countOfAllDocs;
+    size_t countOfAllUniqWords;
+
+
+
+    return 0;
+}
---
+++ da/CP/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[]) {
+    wchar_t kek;
+    std::cout << sizeof(kek) << std::endl;
+    // char x;
+    // std::cin >> x;
+    // std::cout << x << std::endl;
+    // ==============
+    std::string lol2;
+    std::cin >> lol2;
+    std::cout << lol2 << std::endl;    
+    // ==============
+    // std::wstring lol;
+    // std::wcin >> lol;
+    // std::wcout << lol << std::endl;
+    return 0;
+}
